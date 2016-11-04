46b2af0999e834395a1947a3f6613690b86ec44c
Trey Hakanson <treyhakanson@gmail.com>
2016-11-04 00:29:35 -0400

    added routes

---
+++ src/components/your_items.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class YourItems extends Component {
+	render() {
+		return (
+			<div>Your Stuff is here</div>
+		);
+	}
+}
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,21 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import Home from './components/home';
+import Signup from './components/signup';
+import Login from './components/login';
 import AllItems from './components/all_items';
+import YourItems from './components/your_items';
+import AddItems from './components/add_items';
+import Transaction from './components/transaction';
 
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={Home} />
+		<Route path="signup" component={Signup} />
+		<Route path="login" component={Login} />
 		<Route path="all-the-stuff" component={AllItems} />
+		<Route path="your-stuff" component={YourItems} />
+		<Route path="add-stuff" component={AddItems} />
+		<Route path="swap-stuff" component={Transaction} />
 	</Route>
 );
