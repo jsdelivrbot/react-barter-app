fc45190e44f007280241587c56dd1af5c6185909
Trey Hakanson <treyhakanson@gmail.com>
2016-11-04 02:09:00 -0400

    styling coming along on main page. preliminary navbar is in

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+const Navbar = () => {
+	return (
+		<div className="navbar-wrapper">
+			<ul className="navbar-options">
+				<li>All Items</li>
+				<li>Your Items</li>
+				<li>Add Item</li>
+			</ul>
+		</div>
+	);
+}
+
 export default class App extends Component {
   render() {
     return (
       <div>
-	      <div>It is working now</div>
+	      <Navbar />
 	      <div>{this.props.children}</div>
       </div>
     );
