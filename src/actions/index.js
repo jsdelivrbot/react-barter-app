b84407040d1ccf04ad445476334336ceb4b46f85
Trey Hakanson <treyhakanson@gmail.com>
2016-11-04 01:39:07 -0400

    items reducer is connected to an elementary UI

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,11 +17,11 @@ export function fetchItems() {
 	const tmp = {
 		type: FETCH_ITEMS,
 		payload: {
-			1: { id: 1, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'lorempixel.com/400/400/' },
-			2: { id: 2, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'lorempixel.com/400/400/' },
-			3: { id: 3, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'lorempixel.com/400/400/' },
-			4: { id: 4, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'lorempixel.com/400/400/' },
-			5: { id: 5, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'lorempixel.com/400/400/' }
+			1: { id: 1, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'http://www.ebike-evasion.fr/wp-content/uploads/2014/04/placeholder-840x630.png' },
+			2: { id: 2, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'http://www.ebike-evasion.fr/wp-content/uploads/2014/04/placeholder-840x630.png' },
+			3: { id: 3, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'http://www.ebike-evasion.fr/wp-content/uploads/2014/04/placeholder-840x630.png' },
+			4: { id: 4, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'http://www.ebike-evasion.fr/wp-content/uploads/2014/04/placeholder-840x630.png' },
+			5: { id: 5, title: faker.lorem.words(), description: faker.lorem.lines(), imgUrl: 'http://www.ebike-evasion.fr/wp-content/uploads/2014/04/placeholder-840x630.png' }
 		}
 	}
 	return tmp;
--- src/components/all_items.js
+++ src/components/all_items.js
@@ -3,6 +3,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchItems } from '../actions/index';
 
+const ItemContainer = ({ item }) => {
+	return (
+		<div className="item-container">
+			<img src={item.imgUrl} alt={item.title} />
+			<div className="text-center">{item.title}</div>
+		</div>
+	);
+};
+
 class AllItems extends Component {
 	componentWillMount() { // rewatch how to use onEnter to call an action, it wasn't working earlier
 		this.props.fetchItems();
@@ -10,7 +19,16 @@ class AllItems extends Component {
 
 	render() {
 		return (
-			<div>All Items are Here</div>
+			<div>	
+				<div className="text-center">All Items</div>
+				<div className="text-center">Time Remaining: 5 days</div>
+				<div className="all-items">
+						{
+							Object.values(this.props.items).map((item, i) =>
+								<ItemContainer key={i} item={item} />)
+						}
+				</div>
+			</div>
 		);
 	}
 }
